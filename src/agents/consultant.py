41cd0d3cfbaae055b59b30f445f35c5a204741a3
Ryan <r.benatallah01@gmail.com>
2025-05-10 09:16:42 +0100

    (added): oracle on islamic finance as a tool + neo4j query

--- src/agents/consultant.py
+++ src/agents/consultant.py
@@ -1,14 +1,16 @@
 from agno.agent import Agent
+from agno.tools.tavily import TavilyTools
 
 from src.config.prompts import CONSULTANT_INSTRUCTION
 from src.agents import GPT4o
 from src.agents.models import ConsultantResponse
-from agno.tools.tavily import TavilyTools
+from src.tools.consultant import website_kb_as_tool, query_aura
+
 
 consultant = Agent(
 	name="consultant",
 	model=GPT4o,
 	instructions=CONSULTANT_INSTRUCTION,
-	tools=[TavilyTools()],
+	tools=[TavilyTools(), website_kb_as_tool, query_aura],
     response_model=ConsultantResponse,
 )
--- src/tools/consultant.py
+++ src/tools/consultant.py
@@ -1,10 +1,22 @@
 from agno.tools import tool
+from agno.tools.website import WebsiteTools
+
+from src.tools import driver, website_kb
+
+website_kb_as_tool = WebsiteTools(knowledge_base=website_kb)
 
 @tool
-def query_graph_knowledge(query: str) -> str:
-    """
-    Queries the graph knowledge base for the answer to the query.
-    """
-    return "This is a test response"
+def query_aura(cypher_query: str, parameters: dict = None):
+    """Executes a Cypher query on a Neo4j Aura DB and returns results as list of dictionaries."""
+    results = []
 
+    with driver.session() as session:
+        try:
+            result = session.run(cypher_query, parameters or {})
+            results = [record.data() for record in result]
+        except Exception as e:
+            print(f"[Neo4j Error] {e}")
+        finally:
+            driver.close()
 
+    return results
